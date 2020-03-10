3cdbced817484fdc0db779f8e381a2782dd305a6
lnychian <lnychian@outlook.com>
2020-03-11 03:19:52 +0800

    bootcss优化页面|新增分页按钮

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -19,5 +19,9 @@ class Blog(models.Model):
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True)
     last_updated_time = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return '<Blog: %s>' % self.title
+
+    class Meta:
+        ordering = ['created_time']
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
-
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 # Create your views here.
 from .models import Blog, BlogType
 
 
 def blog_list(request):
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list,3)#每3页进行分页
+    page_num =  request.GET.get('page',1)#获取url的页面参数(GET请求)
+    page_of_blogs = paginator.get_page(page_num)
+
     context = {}
-    context['blogs'] = Blog.objects.all()
+    context['page_of_blogs'] = page_of_blogs
+    context['blog_types'] = BlogType.objects.all()
     return render(request, '../templates/blog_list.html', context)
 
 
@@ -21,4 +27,5 @@ def blogs_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blog_types'] = BlogType.objects.all()
     return render(request, '../templates/blogs_with_type.html', context)
