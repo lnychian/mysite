8d33fa0a6e4c7ecd4ceffde83c1324fec229e451
lnychian <lnychian@outlook.com>
2020-03-12 02:51:19 +0800

    翻页|日期归档

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import blog_list, blog_detail, blogs_with_type
+from .views import blog_list, blog_detail, blogs_with_type, blogs_with_date
 
 urlpatterns = [
     path('', blog_list, name='blog_list'),
     path('<int:blog_pk>', blog_detail, name='blog_detail'),
     path('type/<int:blog_type_pk>', blogs_with_type, name='blogs_with_type'),
+    path('<int:year>/<int:month>', blogs_with_date, name='blogs_with_date')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,31 +1,53 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-# Create your views here.
 from .models import Blog, BlogType
 
 
+# Create your views here.
+
+
+# 通用返回参数
+def getBlogCommonList(request, blogs_all_list):
+    context = {}
+    paginator = Paginator(blogs_all_list, 10)  # 每10篇文章进行一次分页
+    page_num = request.GET.get('page', 1)  # 获取url的页面参数(GET请求)
+    context['page_of_blogs'] = paginator.get_page(page_num)
+    context['blog_types'] = BlogType.objects.all()
+    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order='ASC')
+    return context
+
+
+# 全部博客列表请求
 def blog_list(request):
     blogs_all_list = Blog.objects.all()
-    paginator = Paginator(blogs_all_list,3)#每3页进行分页
-    page_num =  request.GET.get('page',1)#获取url的页面参数(GET请求)
-    page_of_blogs = paginator.get_page(page_num)
-
-    context = {}
-    context['page_of_blogs'] = page_of_blogs
-    context['blog_types'] = BlogType.objects.all()
+    context = getBlogCommonList(request, blogs_all_list)
     return render(request, '../templates/blog_list.html', context)
 
 
+# 同type博客页面请求
+def blogs_with_type(request, blog_type_pk):
+    blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
+    blogs_all_list = Blog.objects.filter(blog_type=blog_type)
+    context = getBlogCommonList(request, blogs_all_list)
+    context['blog_type'] = blog_type
+    return render(request, '../templates/blogs_with_type.html', context)
+
+
+# 日期归档页面请求
+def blogs_with_date(request, year, month):
+    blogs_all_list = Blog.objects.filter(created_time__year=year, created_time__month=month)
+    context = getBlogCommonList(request, blogs_all_list)
+    context['blogs_with_date'] = '%s年%s月' % (year, month)
+    return render(request, '../templates/blogs_with_date.html', context)
+
+
+# 单篇博客页面请求
 def blog_detail(request, blog_pk):
     context = {}
-    context['blog'] = get_object_or_404(Blog, pk=blog_pk)
+    blog = get_object_or_404(Blog, pk=blog_pk)
+    previous_blog = Blog.objects.filter(created_time__lt=blog.created_time).last()
+    next_blog = Blog.objects.filter(created_time__gt=blog.created_time).first()
+    context['blog'] = blog
+    context['previous_blog'] = previous_blog
+    context['next_blog'] = next_blog
     return render(request, '../templates/blog_detail.html', context)
-
-
-def blogs_with_type(request, blog_type_pk):
-    context = {}
-    blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
-    context['blog_type'] = blog_type
-    context['blog_types'] = BlogType.objects.all()
-    return render(request, '../templates/blogs_with_type.html', context)
